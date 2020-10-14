ec5588a1bd59d2c2b31b4366e7b373ede0f80243
prashanthMogili <17B81A05E7@CVR.AC.IN>
2020-10-14 07:31:21 +0530

    first

---
+++ build/precache-manifest.7f4ba0ca1b6e33c373ac8e7fa310a83e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f786bf4c24284f879423b3c7489528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74f4c48234bb99874a1",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "0ea1efa68fb30a738f7d",
+    "url": "/static/css/main.fd1f9371.chunk.css"
+  },
+  {
+    "revision": "f74f4c48234bb99874a1",
+    "url": "/static/js/2.58a8ef22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58a8ef22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea1efa68fb30a738f7d",
+    "url": "/static/js/main.4ed64a5b.chunk.js"
+  },
+  {
+    "revision": "b957cec06c6437b7ad73",
+    "url": "/static/js/runtime-main.c7ceb169.js"
+  }
+]);
